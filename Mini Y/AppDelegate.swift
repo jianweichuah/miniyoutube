36131f2aad660fffb60d9d683227dcbba8b4d84b
Jonas <60469464+Jonass-K@users.noreply.github.com>
2021-07-26 10:54:06 +0200

    Converted via safari-web-extension-converter

---
+++ Mini Y/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Mini Y
+//
+//  Created by Jonas Kaiser on 26.07.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
